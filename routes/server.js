85b0f272958c9c02fd90536eaca53d1ac2cb756f
Jose M Bernhardt <Jose M Bernhardt>
2016-10-05 01:31:10 -0400

    Addng weekdays movies, missing schedule

--- routes/server.js
+++ routes/server.js
@@ -17,7 +17,7 @@ nightmare
 	.goto(urlWeb)
 	.wait(selectCity)
 	.select(selectCity, '19')
-	.wait(6000)
+	.wait(8000)
 	.select(selectTheater, '12')
 	.wait(1000)
 	.click(enterBtn)
@@ -27,24 +27,64 @@ nightmare
 			//returning HTML for cheerio
 			return document.body.innerHTML;	
 	})
-	.then(function(body){
-		//loading body to cheerio
-  		var $ = cheerio.load(body);
+	.then(function(bodyDaily){
+		// Loading HTML body on jquery cheerio
+		 var $ = cheerio.load(bodyDaily);
 
-  		//Looping on each div for Carterla para Hoy
+  		//Looping on each div for seccion de Carterla para Hoy
   		$('.showtimeDaily').each(function(index, element){
-  			console.log("---Titulo en Espaniol--");
+  			//spanish title
   			console.log($(this).find('h3').children().text());
-  			console.log("---Titulo en Ingles--");
+  			//english title
   			console.log($(this).find('h4').text());
+  			//schedule for today
+  			console.log($(this).find('li').children().text() + " ");
+  			//img for movie
+  			console.log($(this).find('img').attr('src'));
+  				//show time data such as gender, lenght, language
+  			console.log($(this).find('.showtimeData').text());
+  			var showtimeData = $(this).find('.showtimeData').text();
+  			//console.log(JSON.stringify(showtimeData.replace(/\t|\n/g, "")));
+  		});
+
+  		nightmare
+  			.goto('http://www.palaciodelcine.com.do/info/showtimes/weekly.aspx')
+  			.wait() //TODO have to wait for the DOM to be ready
+  			.evaluate(function(){
+  				return document.body.innerHTML;
+  			})
+  			.then(function(bodyWeekly){
+
+  				var $ = cheerio.load(bodyWeekly);
+  				$('.showtimeWeekly').each(function(index, element){
+  					//movie picture	
+  					console.log($(this).find('.showtimePic').children('img').attr('src'));
+  					//trailer url
+  					console.log($(this).find('.showtimeTrailer').children('h2').children('a').attr('href'));
+  					//spanish title
+  					console.log($(this).find('h3').children('a').text());
+  					//enlish title
+  					console.log($(this).find('h4').text());
+  					//language
+  					console.log($(this).find('strong').text());
+  					//full schedule
+  					$($(this).find('tr')).each(function(i, currentElement){
+  				
+  						if($(this).children().is('th')){
+  							console.log("true");
+  							//TODO scrap info 
+  						}
+  					});
+  				});
+  			});
+
+      		console.log('Done!');
 
 
-  			console.log($(this).find('li').children().text());
-
-  		})
-		//console.log($.html());
+	});
+	
 
 
-
-	})
+	
+	
 	

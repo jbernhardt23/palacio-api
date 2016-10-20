c0d9c6882cfb62835913d7d561039a578dc19ec0
Jose M Bernhardt <Jose M Bernhardt>
2016-10-20 00:14:06 -0400

    more

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type' :
 	'text/html'});
-	res.end("Hello")
+	res.end("Manuel Gay")
 
 });
 

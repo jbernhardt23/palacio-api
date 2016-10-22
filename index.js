51630e53fde95ba60a885c7b7c8c6bee4b43d26b
Jose M Bernhardt <Jose M Bernhardt>
2016-10-22 14:50:32 -0400

    Modifying server

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type' :
-	'text/html'});
+	'application/json'});
 
 	var moviesFile = fs.readFileSync('./moviesData.json', 'utf8');
-	res.end(moviesFile);
+	res.end(JSON.stringify(JSON.parse(moviesFile)));
 
 });
 
 var port = Number(process.env.PORT || 3000)
-server.listen(port);
+server.listen(port + "Listenning");

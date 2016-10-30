bcee2b0cb6c2f00b8d4212fc6f5e2e806a51c3e1
Jose M Bernhardt <Jose M Bernhardt>
2016-10-29 22:47:46 -0400

    More

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res){
-	
-	res.setHeader('Access-Control-Allow-Headers', req.header.origin);
+
+	res.setHeader("Access-Control-Allow-Origin", "*");
+  	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+ 	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+
 	res.writeHead(200, {'Content-Type' :
 	'application/json'});
 
